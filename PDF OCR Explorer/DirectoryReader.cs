77b411bc19e6819913526dd7df26853485447777
yayoimizuha <poko.peko.tomo@icloud.com>
2023-09-15 11:42:18 +0900

    update

---
+++ PDF OCR Explorer/DirectoryReader.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace PDF_OCR_Explorer{
+    public class DirectoryReader{
+        internal string SaveDir;
+
+        public DirectoryReader() {
+            SaveDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "pdf_ocr_explorer");
+            if (Directory.Exists(SaveDir)){
+            }
+            else{
+                Console.WriteLine("Save directory not found:" + SaveDir);
+                Directory.CreateDirectory(SaveDir);
+            }
+        }
+    }
+}
--- PDF OCR Explorer/MainPage.xaml.cs
+++ PDF OCR Explorer/MainPage.xaml.cs
@@ -6,39 +6,44 @@ namespace PDF_OCR_Explorer{
     public partial class MainPage : ContentPage{
         int count = 0;
 
-        private const string Endpoint = "https://tomokazu-katayama-1.cognitiveservices.azure.com/";
-        private const string Key1 = "90536c6541af426eab1c6e8e4a8cdafe";
-        private const string Key2 = "dd1eeaea8843491dadb258f8642b22ec";
+        internal const string Endpoint = "https://tomokazu-katayama-1.cognitiveservices.azure.com/";
+        internal const string Key1 = "90536c6541af426eab1c6e8e4a8cdafe";
+        internal const string Key2 = "dd1eeaea8843491dadb258f8642b22ec";
+        internal DirectoryReader DirectoryReader;
+
 
         public MainPage() {
             var azureKeyCredential = new AzureKeyCredential(Key1);
-            var documentAnalysisClient = new DocumentAnalysisClient(new Uri(Endpoint), azureKeyCredential);
+            var documentAnalysisClient =
+                new DocumentAnalysisClient(new Uri(Endpoint), azureKeyCredential);
             InitializeComponent();
-            try{
-                ThumbnailView.WidthRequest = Math.Min(400, Window.Width * .2);
-            }
-            catch{
-                // ignored
-            }
+
 
             Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
             Label.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
                 "pdf_ocr_explorer");
+            DirectoryReader = new DirectoryReader();
         }
 
 
-//        private void OnCounterClicked(object sender, EventArgs e) {
-//            count++;
-//
-//            if (count == 1)
-//                CounterBtn.Text = $"Clicked {count} time";
-//            else
-//                CounterBtn.Text = $"Clicked {count} times";
-//
-//            SemanticScreenReader.Announce(CounterBtn.Text);
-//        }
-        private void Button_OnClicked(object sender, EventArgs e) {
-            throw new NotImplementedException();
+        private void ThumbnailView_Loaded(object sender, EventArgs e) {
+            ThumbnailView.WidthRequest = Math.Min(400, Window.Width * .2);
         }
+
+        private void Window_SizeChanged(object sender, EventArgs e) {
+            ThumbnailView_Loaded(sender, e);
+        }
+
+
+        //        private void OnCounterClicked(object sender, EventArgs e) {
+        //            count++;
+        //
+        //            if (count == 1)
+        //                CounterBtn.Text = $"Clicked {count} time";
+        //            else
+        //                CounterBtn.Text = $"Clicked {count} times";
+        //
+        //            SemanticScreenReader.Announce(CounterBtn.Text);
+        //        }
     }
 }
