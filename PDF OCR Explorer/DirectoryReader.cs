9f33814bc229bc4de36905111b75a52a4046e05c
yayoimizuha <poko.peko.tomo@icloud.com>
2023-09-20 12:22:50 +0900

    update

--- PDF OCR Explorer/DirectoryReader.cs
+++ PDF OCR Explorer/DirectoryReader.cs
@@ -5,41 +5,41 @@ using Windows.Data.Pdf;
 #endif
 
 
-namespace PDF_OCR_Explorer{
-    public class DirectoryReader{
-        internal readonly string SaveDir;
-        internal readonly string DataDir;
-        internal List<string> Files;
+namespace PDF_OCR_Explorer;
 
-        public DirectoryReader() {
-            SaveDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "pdf_ocr_explorer");
-            if (Directory.Exists(SaveDir)){
-            }
-            else{
-                Console.WriteLine("Save directory not found: " + SaveDir);
-                Directory.CreateDirectory(SaveDir);
-            }
+public class DirectoryReader{
+    internal readonly string SaveDir;
+    internal readonly string DataDir;
+    internal List<string> Files;
 
-            DataDir = Path.Combine(SaveDir, "files");
-            if (Directory.Exists(DataDir)){
-            }
-            else{
-                Console.WriteLine("Save directory not found: " + DataDir);
-                Directory.CreateDirectory(DataDir);
-            }
-
-            Console.WriteLine(Directory.GetDirectories(DataDir));
-            Files = new List<string>(Directory.GetFiles(DataDir));
+    public DirectoryReader() {
+        SaveDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "pdf_ocr_explorer");
+        if (Directory.Exists(SaveDir)){
+        }
+        else{
+            Console.WriteLine("Save directory not found: " + SaveDir);
+            Directory.CreateDirectory(SaveDir);
         }
 
-        internal short DocumentAdd(string filePath) {
-            File.Copy(filePath,
-                Path.Combine(DataDir, Files.Count + Path.GetExtension(Path.GetFileName(filePath))));
-            if (Path.GetExtension(filePath).ToLower().Equals(".pdf")){
-            }
-
-            Files.Add(filePath);
-            return 0;
+        DataDir = Path.Combine(SaveDir, "files");
+        if (Directory.Exists(DataDir)){
         }
+        else{
+            Console.WriteLine("Save directory not found: " + DataDir);
+            Directory.CreateDirectory(DataDir);
+        }
+
+        Console.WriteLine(Directory.GetDirectories(DataDir));
+        Files = new List<string>(Directory.GetFiles(DataDir));
+    }
+
+    internal short DocumentAdd(string filePath) {
+        File.Copy(filePath,
+            Path.Combine(DataDir, Files.Count + Path.GetExtension(Path.GetFileName(filePath))));
+        if (Path.GetExtension(filePath).ToLower().Equals(".pdf")){
+        }
+
+        Files.Add(filePath);
+        return 0;
     }
 }
--- PDF OCR Explorer/MainPage.xaml.cs
+++ PDF OCR Explorer/MainPage.xaml.cs
@@ -2,8 +2,8 @@
 using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Azure;
 
-namespace PDF_OCR_Explorer {
-    public partial class MainPage : ContentPage {
+namespace PDF_OCR_Explorer{
+    public partial class MainPage : ContentPage{
         private readonly IFilePicker _filePicker;
         int _count = 0;
 
@@ -12,10 +12,37 @@ namespace PDF_OCR_Explorer {
         internal const string Key2 = "dd1eeaea8843491dadb258f8642b22ec";
         internal DirectoryReader DirectoryReader;
 
-        public class Thumbnail {
+        public class Thumbnail{
             public string FileName { get; set; }
             public string OrigFile { get; set; }
             public byte ThumbData { get; set; }
+        }
+
+        private static Grid AddThumb(string fp) {
+            var grid = new Grid {
+                HeightRequest = 400,
+                RowDefinitions = {
+                    new RowDefinition { Height = new GridLength(75) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                },
+                ColumnDefinitions = {
+                    new ColumnDefinition()
+                }
+            };
+            grid.Add(
+                new Label {
+                    Text = Path.GetFileName(fp),
+                    VerticalOptions = LayoutOptions.End,
+                    FontSize = 30
+                }
+                , 0, 0
+            );
+            grid.Add(
+                new BoxView {
+                    Color = Colors.Orange
+                }, 0, 1
+            );
+            return grid;
         }
 
         public MainPage() {
@@ -27,10 +54,10 @@ namespace PDF_OCR_Explorer {
 
             Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
             DirectoryReader = new DirectoryReader();
-            Label.Text = string.Join('\n', DirectoryReader.Files);
-            foreach (var filePath in DirectoryReader.Files) {
+            Label.Text = string.Join(Environment.NewLine, DirectoryReader.Files);
+            foreach (var filePath in DirectoryReader.Files){
                 ThumbnailStack.Children.Add(
-                    new Grid { }
+                    AddThumb(fp: filePath)
                     //new ImageButton { MinimumHeightRequest = 400, Margin = new Thickness(20), Source = filePath }
                 );
             }
@@ -45,24 +72,30 @@ namespace PDF_OCR_Explorer {
             ThumbnailView_Loaded(sender, e);
         }
 
+
         private readonly PickOptions _pickOptions = new() {
             PickerTitle = "スキャンするファイルを選択してください。",
             FileTypes = new FilePickerFileType(
                 new Dictionary<DevicePlatform, IEnumerable<string>> {
-                    { DevicePlatform.WinUI, new[] { ".pdf", ".jpg", ".png" } },
+                    { DevicePlatform.WinUI, FilePickerFileType.Images.Value.Append(".pdf") },
                 })
         };
 
         private async void FilePickerButton_OnClicked(object sender, EventArgs e) {
             var pickerRes = await FilePicker.PickMultipleAsync(_pickOptions);
             var addFiles = "";
-            foreach (var fileResult in pickerRes) {
-                Label.Text += fileResult.FullPath + Environment.NewLine;
+            foreach (var fileResult in pickerRes){
+                Label.Text += Environment.NewLine + fileResult.FullPath;
                 addFiles += fileResult.FileName + Environment.NewLine;
                 DirectoryReader.DocumentAdd(fileResult.FullPath);
+                ThumbnailStack.Children.Add(
+                    AddThumb(fileResult.FullPath)
+                );
             }
 
-            await DisplayAlert("追加されたファイル", addFiles, "OK");
+            if (addFiles.Length != 0){
+                await DisplayAlert("追加されたファイル", addFiles, "OK");
+            }
         }
     }
 }
