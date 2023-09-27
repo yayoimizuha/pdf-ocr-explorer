4d4142e6cd0a066225f39452777091d78b1b7596
yayoimizuha <poko.peko.tomo@icloud.com>
2023-09-28 08:47:13 +0900

    update

--- PDF OCR Explorer/SearchView.xaml.cs
+++ PDF OCR Explorer/SearchView.xaml.cs
@@ -7,7 +7,7 @@ using iText.Svg.Renderers.Path.Impl;
 
 namespace PDF_OCR_Explorer;
 
-public partial class SearchView : ContentPage{
+public partial class SearchView : ContentPage {
     private readonly Manager.PoeFile _poeFile;
 
     public SearchView(Lines lines, int pageNum, Manager.PoeFile poeFile, double angle) {
@@ -16,8 +16,10 @@ public partial class SearchView : ContentPage{
         pageNum++;
         Header.Text = _poeFile.DispName + $" {pageNum}ページ目";
         string base64DataUri = null;
-        if (Path.GetExtension(_poeFile.OrigFile)!.ToLower().Equals(".pdf")){
-            var pdfDocument = new PdfDocument(new PdfReader(_poeFile.FilePath));
+        if (Path.GetExtension(_poeFile.OrigFile)!.ToLower().Equals(".pdf")) {
+            var reader = new PdfReader(_poeFile.FilePath);
+            reader.SetUnethicalReading(true);
+            var pdfDocument = new PdfDocument(reader);
             var outputStream = new MemoryStream();
             var pdfWriter = new PdfWriter(outputStream);
             pdfWriter.SetCloseStream(false);
@@ -25,14 +27,21 @@ public partial class SearchView : ContentPage{
             pdfDocument.CopyPagesTo(pageNum, pageNum, outputDocument);
             //pdfDocument.GetPage(pageNum).SetRotation((int)Math.Round(angle / 90) * 90).CopyTo(outputDocument);
             outputDocument.GetPage(1).SetRotation((int)(Math.Round(angle / 90) * -90));
+
             var canvas = new PdfCanvas(outputDocument.GetPage(1));
-            canvas.SetStrokeColor(ColorConstants.BLUE)
-                .MoveTo(0,0).LineTo(50,50).Stroke();
-            // foreach (var polygon in lines.BoundingPolygon[1..]){
-            //     canvas.LineTo(polygon.X, polygon.Y);
-            // }
+            //canvas.SetStrokeColor(ColorConstants.BLUE)
+            //    .SetLineWidth(outputDocument.GetPage(1).GetPageSize().GetX() / 100f)
+            //    .MoveTo(lines.BoundingPolygon.First().X, lines.BoundingPolygon.First().Y)
+            //    .LineTo(lines.BoundingPolygon[1].X, lines.BoundingPolygon[1].Y).ClosePathStroke();
+            canvas.SetStrokeColor(ColorConstants.CYAN)
+                .SetLineWidth(outputDocument.GetPage(1).GetPageSize().GetHeight() / 100000)
+                .MoveTo(0, 0).LineTo(0.1, 0.1).ClosePathStroke();
+            //foreach (var polygon in lines.BoundingPolygon[1..]) {
+            //    canvas.LineTo(polygon.X, polygon.Y);
+            //}
+
             // canvas.Stroke();
-                
+
 
             outputDocument.Close();
             pdfWriter.Flush();
@@ -42,7 +51,7 @@ public partial class SearchView : ContentPage{
             base64DataUri = "data:application/pdf;base64," + Convert.ToBase64String(outputStream.ToArray());
             //Debug.Write(base64DataUri);
         }
-        else{
+        else {
             base64DataUri = $"data:image/{Path.GetExtension(_poeFile.FilePath)!.Replace(".", "")};base64," +
                             Convert.ToBase64String(
                                 File.ReadAllBytes(_poeFile.FilePath ?? throw new FileNotFoundException()));
